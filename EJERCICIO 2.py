f5ecf0401254f87898eab8f8b0ee5c7944394e1e
escalante-camila <130803439+escalante-camila@users.noreply.github.com>
2023-09-14 20:30:27 -0300

    EJERCICIO 2.py

--- EJERCICIO 2.py
+++ EJERCICIO 2.py
@@ -1,12 +1,11 @@
 print("Números primos en un rango")
 
-
-rango = []
+numeros = []
 continuar = 1
 
 while continuar == 1:
     numero = int(input("Ingrese un número para el arreglo: "))
-    rango.append(numero)
+    numeros.append(numero)
     continuar = int(input("Si desea seguir agregando números seleccione <1>: "))
 
 
@@ -14,21 +13,21 @@ def numero_primo(num):
     primo = False
     if num >= 1 and num <= 3:
         primo = True
-    elif num2 % 2 != 0 or num % 3 != 0:
+    elif num % 2 != 0 or num % 3 != 0:
         primo = True
     return primo
 
-def suma_numeros_primos(rango):
+def suma_numeros_primos(numeros):
     suma_primos = 0
     num_primos = 0
-    for rang in rango:
-        if num_primos(rango[rang]):
-            suma_numeros_primos += rang
+    for numero in numeros:
+        if numero_primo(numero):
+            suma_numeros_primos += numero
             num_primos += 1
     return ("El resultado de la suma de los números primos del arreglo ingresado es: ", suma_numeros_primos)
 
 realizar = int(input("Ingrese 1 si quiere realizar la suma de los números primos del rango ingresado: "))
 
 if realizar == 1:
-    suma = suma_numeros_primos(rango)
+    suma = suma_numeros_primos(numeros)
     print ("Resultado: ", suma)
