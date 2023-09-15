8f7ee3fe93e92308866df0eca3fbfd70c347fa0b
escalante-camila <130803439+escalante-camila@users.noreply.github.com>
2023-09-14 21:05:48 -0300

    EJERCICIO 2.py

--- EJERCICIO 2.py
+++ EJERCICIO 2.py
@@ -1,6 +1,3 @@
-print("Números primos en un rango")
-
-
 numeros = []
 continuar = 1
 
@@ -10,12 +7,19 @@ while continuar == 1:
     continuar = int(input("Si desea seguir agregando números seleccione <1>: "))
 
 def numero_primo(num):
+    primo = False
     num_primos = []
     for num in numeros:
-        if num > 1 and num <= 3:
+        if num == 2 or num == 3:
             num_primos.append(num)
+            primo = True
+        elif num == 1:
+            primo = False
         elif num % 2 != 0 or num % 3 != 0:
             num_primos.append(num)
+            primo = True
+        else: 
+            print("No es primo.")
     return num_primos
 
 realizar = int(input("Ingrese 1 ver los números primos del arreglo: "))
@@ -23,3 +27,5 @@ realizar = int(input("Ingrese 1 ver los números primos del arreglo: "))
 if realizar == 1:
     mostrar = numero_primo(numeros)
     print (mostrar)
+else: 
+    print("Fin del programa.")
